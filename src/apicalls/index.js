a345fbdb5c374c16cfe9a55a1294530a8d886fba
Yekaditya Devadi <yekaditya33@gmail.com>
2024-03-05 12:48:48 +0530

    update password

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://quiz-portal-mern-server.vercel.app",
+  baseURL: "https://quiz-portal-mern-server.vercel.app",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
